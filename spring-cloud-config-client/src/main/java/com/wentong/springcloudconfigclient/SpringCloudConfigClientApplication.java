43911355f24740b2561d0e33595518e63d358c04
周文童 <zhouwentong@finupgroup.com>
2019-07-10 15:54:42 +0800

    config-server & config-client

---
+++ spring-cloud-config-client/src/main/java/com/wentong/springcloudconfigclient/SpringCloudConfigClientApplication.java
@@ -0,0 +1,26 @@
+package com.wentong.springcloudconfigclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringCloudConfigClientApplication {
+
+    @Value("${env}")
+    String value;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigClientApplication.class, args);
+    }
+
+    @GetMapping("test")
+    public String test() {
+        return value;
+    }
+
+
+}
