d3ab3d651ae11f70b5b3f2cf7cf3e0680dfa8041
周文童 <zhouwentong@finupgroup.com>
2019-07-10 16:21:19 +0800

    将 config server & config client 注册到 Eureka 上，方便管理

--- spring-cloud-config-client/src/main/java/com/wentong/springcloudconfigclient/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/wentong/springcloudconfigclient/SpringCloudConfigClientApplication.java
@@ -3,11 +3,13 @@ package com.wentong.springcloudconfigclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class SpringCloudConfigClientApplication {
 
     @Value("${env}")
